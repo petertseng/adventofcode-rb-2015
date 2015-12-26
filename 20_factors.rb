51a6c90a20b964c3672c9779d5c6df227850d1c9
Peter Tseng <pht24@cornell.edu>
2015-12-26 19:08:58 +0000

    day 20: Don't multiply gifts given
    This slightly speeds things up (17 -> 15 seconds), though there are
    obviously many more gains to be had in other areas.
    
    Using prime factorisation and the formula to calculate the factor sum
    given the prime factorisation turned out to slow things down (to > 30
    seconds) so that will not be the path to take.
    
    Probably the next step to take is to establish an upper bound on the
    house number - this is the reason part 2 runs so much faster than part 1
    (4 seconds as opposed to 13).

--- 20_factors.rb
+++ 20_factors.rb
@@ -8,8 +8,8 @@ def give_gifts(target, multiplier, elf_limit: nil)
     nums = (elf..max).step(elf)
     nums = nums.take(elf_limit) if elf_limit
     nums.each_with_index { |house, i|
-      total_gifts = (gifts[house] += elf * multiplier)
-      if total_gifts >= target
+      total_gifts = (gifts[house] += elf)
+      if total_gifts >= max
         best = [best, house].min
         # If it's my first house, no later elf can undercut me.
         return best if i == 0
