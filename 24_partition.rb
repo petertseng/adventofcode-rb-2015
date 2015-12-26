69b6ed1cdfab7ff69d2e5635f407b0309ee3edfb
Peter Tseng <pht24@cornell.edu>
2015-12-26 11:57:05 +0000

    day 24: Check partitionability
    This is necessary for a solution that is guaranteed to be correct in all
    cases. The previous solution did not bother checking that the remaining
    gifts could be partitioned. This uses Complete Karmarkar-Karp to check
    that a partition exists.
    
    Runtime cost is quite small: 0.7 seconds -> 1.0 seconds, a small price
    to pay for ensuring that the solution is correct.

--- 24_partition.rb
+++ 24_partition.rb
@@ -1,3 +1,5 @@
+require_relative 'lib/ckk'
+
 verbose = ARGV.delete('-v')
 
 weights = ARGF.each_line.map(&method(:Integer)).freeze
@@ -11,13 +13,27 @@ def min_needed(weights, target)
   }.size + 1
 end
 
+module DeleteFirst; refine Array do
+  # Yes, so evil I am, redefining - on arrays.
+  # It's needed in partition, because I want to delete exactly one element.
+  # So, [1, 1, 2] - [1] should == [1, 2]
+  def -(other)
+    arr = dup
+    other.each { |x| arr.delete_at(arr.index(x) || arr.length) }
+    arr
+  end
+end; end
+
+using DeleteFirst
+
 def partition(weights, num_groups)
   sum = weights.sum
   each_group = sum / num_groups
 
   (min_needed(weights, each_group)..(weights.size / num_groups)).each { |n|
-    # Is it sound to assume that the other presents can be divided?!
-    winning_combos = weights.combination(n).select { |c| c.sum == each_group }
+    winning_combos = weights.combination(n).select { |c|
+      c.sum == each_group && CKK[num_groups - 1].new(weights - c).best == 0
+    }
     next if winning_combos.empty?
     return winning_combos.min_by { |c| c.reduce(:*) }
   }
