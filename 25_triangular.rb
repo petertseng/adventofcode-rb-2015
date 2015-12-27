de37a1ccc5f22240f865b9be24e28736e70a000c
Peter Tseng <pht24@cornell.edu>
2015-12-27 23:02:18 +0000

    day 25: Use exponentiation by squaring
    From 2 seconds to 0.08

--- 25_triangular.rb
+++ 25_triangular.rb
@@ -16,7 +16,26 @@ def iterations(row: 1, column: 1)
   (diagonal * diagonal + diagonal) / 2 - row
 end
 
+# Raise base to the power of exp by squaring.
+# No longer necessary since Ruby added modular exponentiation in 2.5,
+# and it is equally as fast.
+# Keeping for posterity.
+def mod_pow(base, exp, mod)
+  return 1 if exp == 0
+
+  odds = 1
+  evens = base
+
+  while exp >= 2
+    odds = odds * evens % mod if exp % 2 == 1
+    evens = evens * evens % mod
+    exp /= 2
+  end
+
+  evens * odds % mod
+end
+
 n = iterations(row: row, column: column)
-x = SEED
-n.times { x = x * BASE % MODULUS }
-puts x
+# This works because a*b%m == (a%m)*(b%m)%m
+#puts SEED * mod_pow(BASE, n, MODULUS) % MODULUS
+puts SEED * BASE.pow(n, MODULUS) % MODULUS
