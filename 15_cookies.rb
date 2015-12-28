ff6cee084bb08687ac1aaabac0edc3da1bc8e264
Peter Tseng <pht24@cornell.edu>
2015-12-28 14:14:05 +0000

    day 15: terminate early if a cookie is doomed
    Now that we are pre-calculating the trait value vectors, we are able to
    see when we cause a cookie to be doomed, and we can terminate early.
    
    By "doomed", we mean that one trait value is so negative that the cookie
    can't be saved even if the remaining ingredients are all the best
    ingredient for that trait.
    
    Speed up from about 1.1 seconds -> 0.8 seconds

--- 15_cookies.rb
+++ 15_cookies.rb
@@ -10,7 +10,16 @@ ingredients = ARGF.each_line.to_h { |line|
 }.freeze
 traits = ingredients.each_value.flat_map(&:keys).uniq.freeze
 
-def cookie(ingredients, index, remaining, traits_so_far, results, amounts)
+# If any trait is negative and can't be saved
+# (not even by putting all of the best ingredient for that trait),
+# the cookie is doomed and will have a zero score.
+def doomed_traits(trait_values, remaining, trait_bests)
+  trait_values.select { |trait, value|
+    value < 0 && -value >= remaining * trait_bests[trait]
+  }
+end
+
+def cookie(ingredients, index, remaining, traits_so_far, trait_bests, results, amounts)
   ingredient = ingredients[index]
 
   if index == ingredients.size - 1
@@ -29,13 +38,39 @@ def cookie(ingredients, index, remaining, traits_so_far, results, amounts)
     traits_with = traits_so_far.merge(ingredient) { |_, v1, v2|
       v1 + amount * v2
     }
+
+    doomed_traits = doomed_traits(traits_with, remaining - amount, trait_bests[index])
+
+    unless doomed_traits.empty?
+      if doomed_traits.any? { |k, _| ingredient[k] <= 0 }
+        # Adding more of the ingredient dooms the cookie further.
+        break
+      else
+        # Adding more of the ingredient helps a trait that is otherwise doomed.
+        # We can calculate a minimum amount of this ingredient,
+        # but I don't feel like writing that code.
+        next
+      end
+    end
+
     amounts[index] = amount
-    cookie(ingredients, index + 1, remaining - amount, traits_with, results, amounts)
+    cookie(ingredients, index + 1, remaining - amount, traits_with, trait_bests, results, amounts)
   }
 end
 
+# Process ingredient with most-negative trait values first.
+# That way we have the greatest chance of eliminating cookies in outer loops.
+# That would reduce the amount of work we need to do.
+ingredients = ingredients.sort_by { |_, v| v.values.min }
+trait_bests = ingredients.each_index.map { |i|
+  traits.to_h { |trait|
+    [trait, ingredients.drop(i + 1).map { |_, ingred| ingred[trait] }.max]
+  }
+}
+ingredients = ingredients.to_h
+
 results = {best: [0, nil], best500: [0, nil]}
-cookie(ingredients.values, 0, 100, traits.to_h { |t| [t, 0] }, results, ingredients.map { nil })
+cookie(ingredients.values, 0, 100, traits.to_h { |t| [t, 0] }, trait_bests, results, ingredients.map { nil })
 
 results.values_at(:best, :best500).each { |score, list|
   puts score
