1ec5e4bb4c40ef83974890e3947528e1d32e1228
Peter Tseng <pht24@cornell.edu>
2015-12-29 05:29:09 +0000

    day 11: Skip passwords that don't meet any of the requirements
    We should avoid blindly cycling through passwords that have no chance of
    working. It should always be the case that passwords meet at least one
    of the three requirements, and we have functions that take a password
    and find the next password that meets a given requirement. Then we just
    repeatedly call these functions instead of the normal #succ function
    
    Speeds up from ~2.2 seconds -> ~0.1 seconds
    To give an idea of how much speedup was given for each check:
    
    Replacing all functions with password.succ!: ~2.4 seconds
    That indicates there's some overhead here.
    
    Use only make_pair! without the two-at-a-time branch: ~0.6 seconds.
    Use both branches of make_pair!: ~0.2 seconds.
    Use only succ_index!: ~2.2 seconds.
    Use only make_ascending!: ~1.9 seconds.
    
    Obviously the pairs requirement got the biggest mileage, because a lot
    of passwords can fail this requirement.

--- 11_passwords.rb
+++ 11_passwords.rb
@@ -1,22 +1,200 @@
 module Password
+  PAIR = /(.)\1/
+
   ASCEND = Regexp.union(*(?a..?z).each_cons(3).map(&:join))
+  # Strings that can be the prefix of a straight
+  ASCEND_PREFIX2 = Regexp.union(*(?a..?y).each_cons(2).map(&:join))
 end
 
 module Password; refine String do
-  def confusing?
-    match?(/i|o|l/)
+  def next_password
+    dup.next_password!
   end
 
-  def ascending?
-    match?(ASCEND)
+  def next_password!
+    succ!
+
+    loop {
+      pairs = []
+      scan(PAIR) { |s|
+        pairs << [s, Regexp.last_match.offset(0).first]
+      }
+
+      # SAFETY:
+      # An unsafe pair is one that:
+      # 1) Is the only pair in the password, and
+      # 2) MUST be destroyed before finding another pair.
+      # For pairs, this means any pairs starting three from the end, or later.
+      # The xx in abcxx and abcxxd are unsafe, the xx in abcxxde is safe.
+      # If we have an unsafe pair, the next password will not include it.
+      # That means we can pretend it's not there.
+      if pairs.empty? || pairs.size == 1 && pairs[0][1] > length - 4
+        # The first string that would generate two pairs:
+        # One pair in the all-but-last-two prefix.
+        # One pair in the suffix, the last two (aa)
+        prefix = self[0...-2].make_pair!
+        replace(prefix + 'aa')
+        next
+      elsif pairs.uniq(&:first).size < 2
+        # Just need one more pair.
+        make_pair!
+        next
+      end
+
+      if (i = index(/i|o|l/))
+        succ_index!(i)
+        next
+      end
+
+      unless match?(ASCEND)
+        make_ascending!
+        next
+      end
+
+      return self
+    }
   end
 
-  def two_repeats?
-    match?(/(.)\1.*(.)\2/)
+  # Increments self at least once.
+  # Stops upon reaching the lowest lexicographic string that has a pair.
+  # (This one's public because it gets called on a prefix to make two pairs)
+  def make_pair!
+    last_was_z = self[-1] == ?z
+
+    succ!
+
+    # Return now for cbz -> cca but NOT aaa -> aab
+    return self if last_was_z && self[-3] == self[-2]
+
+    # We don't need to check more than 3 chars.
+    # The only way >= 3 chars change is if at least the last two chars are z.
+    # In that case, succ! would give a string ending in aa, and that's a pair.
+    # For the same reason, it's OK that we didn't return early for dczz -> ddaa.
+
+    if self[-2] > self[-1]
+      # Easy case: only the last char changes.
+      # For example, cba -> cbb
+      self[-1] = self[-2]
+    elsif self[-2] < self[-1]
+      # The last two chars have to change. Two cases:
+      if self[-3].ord == self[-2].ord + 1
+        # The third-to-last and second-to-last character make a pair.
+        # For example, cbc -> cca (which comes before ccc)
+        self[-2] = self[-3]
+        self[-1] = ?a
+      else
+        # The second-to-last and last character make a pair.
+        # For example, cab -> cbb (which comes before cca)
+        self[-2..-1] = self[-2].succ * 2
+        # It's impossible for second-to-last to be 'z'.
+        # No letter sorts after 'z'.
+        # So we won't accidentally turn a 'z' into an 'aa' this way.
+      end
+    end
+
+    self
   end
 
-  def good?
-    two_repeats? && !confusing? && ascending?
+  private
+
+  # Increments the string at the specified index.
+  # All later indices will change to "a".
+  def succ_index!(i)
+    self[i] = self[i].succ
+    self[(i + 1)..-1] = ?a * (length - 1 - i)
+    self
+  end
+
+  # Increments self at least once.
+  # Stops upon reaching the lowest lexicographic string that ascends.
+  def make_ascending!
+    succ!
+
+    return self if match?(ASCEND)
+
+    # Changes involving the last character:
+    return self if make_straight_end!(-1)
+
+    # Changes involving the second-to-last character:
+    if make_straight_end!(-2)
+      self[-1] = ?a
+      return self
+    end
+    return self if make_straight_middle!(-2)
+
+    # Changes involving the third-to-last character:
+    choices = straight_choices(-3)
+    choices[:end] << 'aa' if choices[:end]
+    choices[:middle] << 'a' if choices[:middle]
+
+    unless choices.empty?
+      self[-3..-1] = choices.values.min
+      return self
+    end
+
+    # Changes involving the fourth-to-last character:
+    prefix = self[0...-3].succ
+    replace(prefix + (prefix.match?(ASCEND) ? 'aaa' : 'abc'))
+
+    self
+  end
+
+  # Given an index that needs to increment its character,
+  # returns Hash[Position => String] of possible straight choices.
+  # Position is :begin | :middle | :end
+  # Strings only contain as many characters as required to make the straight.
+  # Depending on the application, you may need to post-process these.
+  def straight_choices(idx)
+    choices = {}
+
+    if self[idx - 1] == self[idx]
+      if self[(idx - 2)..(idx - 1)].match?(ASCEND_PREFIX2)
+        choices[:end] = self[idx - 1].succ
+      end
+      if increments_to_straight_middle?(idx)
+        s = self[idx - 1].succ
+        s << s.succ
+        choices[:middle] = s
+      end
+    end
+
+    if self[idx] < ?x
+      choices[:begin] = straight_from(self[idx].succ)
+    end
+
+    choices
+  end
+
+  # If possible, makes idx the middle of a straight.
+  # This modifies idx + 1.
+  # Returns self on success, nil on failure.
+  def make_straight_middle!(idx)
+    if self[idx - 1] >= self[idx] && increments_to_straight_middle?(idx)
+      self[idx] = self[idx - 1].succ
+      self[idx + 1] = self[idx].succ
+      self
+    end
+  end
+
+  # If possible, makes idx the end of a straight.
+  # Returns self on success, nil on failure.
+  def make_straight_end!(idx)
+    if self[idx - 1] >= self[idx] && self[(idx - 2)..(idx - 1)].match?(ASCEND_PREFIX2)
+      self[idx] = self[idx - 1].succ
+      self
+    end
+  end
+
+  # Could this string start a straight at idx-1 if idx is incremented?
+  def increments_to_straight_middle?(idx)
+    self[idx - 1] < ?x || self[idx - 1] == ?x && self[idx] < ?y
+  end
+
+  def straight_from(char)
+    s = char
+    s << (second = s.succ)
+    s << second.succ
+    s
   end
 end; end
 
@@ -26,7 +204,6 @@ password = !ARGV.empty? && !File.exist?(ARGV.first) ? ARGV.first : ARGF.read
 password = password.dup
 
 2.times {
-  password.succ!
-  password.succ! until password.good?
+  password = password.next_password
   puts password
 }
