e784abd5ddafb14dbbc58b1950968afb77d569ad
Peter Tseng <pht24@cornell.edu>
2015-12-29 20:47:12 +0000

    day 06: Use symbols
    Pretty insignificant speedup; *maybe* a ~5.4 -> ~5.3 second speedup, but
    I'm not even sure of that. However, I'll comparing more strings in an
    upcoming commit, so this will be useful then.

--- 06_light_grid.rb
+++ 06_light_grid.rb
@@ -4,17 +4,17 @@ def new_grid; Array.new(1000) { Array.new(1000, 0) }; end
 
 puts ARGF.each_line.with_object([new_grid, new_grid]) { |l, (on_off_grid, bright_grid)|
   captures = COMMAND.match(l).captures
-  command = captures.shift
+  command = captures.shift.to_sym
   xmin, ymin, xmax, ymax = captures.map(&method(:Integer))
 
   case command
-  when 'on'
+  when :on
     on_off_result = ->(_) { 1 }
     bright_result = ->(x) { x + 1 }
-  when 'off'
+  when :off
     on_off_result = ->(_) { 0 }
     bright_result = ->(x) { x == 0 ? 0 : x - 1 }
-  when 'toggle'
+  when :toggle
     on_off_result = ->(x) { 1 - x }
     bright_result = ->(x) { x + 2 }
   end
