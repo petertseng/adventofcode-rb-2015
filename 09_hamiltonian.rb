c5bac5b4f8cfe13ebd1240d3093ae12d424d950a
Peter Tseng <pht24@cornell.edu>
2016-01-01 16:10:55 +0000

    day 09: Use Hamiltonian module
    Runtime 0.4 seconds -> 0.1 seconds, not bad.

--- 09_hamiltonian.rb
+++ 09_hamiltonian.rb
@@ -1,3 +1,5 @@
+require_relative 'lib/hamiltonian'
+
 verbose = ARGV.delete('-v')
 
 distances = ARGF.each_line.with_object(Hash.new { |h, k| h[k] = Hash.new(0) }) { |l, dists|
@@ -8,20 +10,7 @@ distances = ARGF.each_line.with_object(Hash.new { |h, k| h[k] = Hash.new(0) }) {
   dists[place2][place1] = dist
 }
 
-distances.keys.permutation.with_object(
-  min: {dist: Float::INFINITY, path: [], accept: :<.to_proc},
-  max: {dist: 0, path: [], accept: :>.to_proc},
-) { |p, answer|
-  dist = p.each_cons(2).sum { |place1, place2|
-    distances[place1][place2]
-  }
-  answer.values.each { |ans|
-    if ans[:accept][dist, ans[:dist]]
-      ans[:dist] = dist
-      ans[:path] = p
-    end
-  }
-}.each_value { |v|
-  puts v[:dist]
-  puts v[:path].join(', ') if verbose
+Graph.paths(distances).values_at(:min, :max).each { |best|
+  puts best[:cost]
+  puts best[:path].join(', ') if verbose
 }
