b436d0a8b1d0ae6227cc1ecb11598439e9b4f406
Peter Tseng <pht24@cornell.edu>
2021-12-27 10:59:00 +0000

    day 20: Use sum of divisors formula
    40x speedup

--- 20_factors.rb
+++ 20_factors.rb
@@ -1,123 +1,43 @@
-require 'prime'
-
 target = Integer(!ARGV.empty? && ARGV.first.match?(/^\d+$/) ? ARGV.first : ARGF.read)
 
-# sigma1 calculates the sum of the factors of n.
-def sigma1(n)
-  Prime.prime_division(n).map { |prime, power|
-    (0..power).sum { |pow| prime ** pow }
-  }.reduce(1, :*)
-end
+PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47].reverse.freeze
 
-def gifts_delivered(house, elf_limit)
-  gifts = 0
-  (1..(house ** 0.5)).each { |candidate|
-    next if house % candidate != 0
-    factor1 = candidate
-    factor2 = house / candidate
-    gifts += factor1 if factor2 < elf_limit
-    gifts += factor2 if factor1 < elf_limit && factor1 != factor2
-  }
-  gifts
-end
+# askalski's tip:
+# https://www.reddit.com/r/adventofcode/comments/po1zel/comment/hd1esc2
+# > The idea behind the recursion is to try including different powers of each prime `p` in the house number,
+# > then solve the subproblem of `ceil(goal / (1 + p + ... + p^n))` using only those primes smaller than `p`.
+@cache = {}
+def sum_exceeds(goal, primes = PRIMES)
+  return goal if primes.empty?
 
-def smallest_greater_factorial(target, valid_bound)
-  factorial = 2
-  n = 2
-  until valid_bound[factorial]
-    n += 1
-    factorial *= n
-  end
-  [factorial, n]
-end
+  @cache[[goal, primes[0]]] ||= begin
+    # try skipping this prime
+    best = sum_exceeds(goal, primes[1..])
 
-def house_upper_bound(target, elf_limit: nil)
-  if elf_limit
-    valid_bound = ->(x) { gifts_delivered(x, elf_limit) >= target }
-  else
-    valid_bound = ->(x) { sigma1(x) >= target }
-  end
-  bound, n = smallest_greater_factorial(target, valid_bound)
+    prime = primes[0]
+    prime_power = 1
+    prime_sum = 1
 
-  # Try to decrease each factor as far as it can go:
-  n.downto(1) { |factor_to_decrease|
-    bound_without = bound / factor_to_decrease
-    (1...factor_to_decrease).each { |replacement|
-      new_bound = bound_without * replacement
-      if valid_bound[new_bound]
-        bound = new_bound
-        break
-      end
-    }
-  }
+    while prime_sum < goal
+      prime_power *= prime
+      prime_sum += prime_power
 
-  bound
-end
-
-# Euler-Mascheroni constant
-GAMMA = 0.57721566490153286060651209008240243104215933593992
-
-def house_lower_bound(target, upper)
-  # Robin's inequality:
-  # \sigma(n) < e^\gamma n \log \log n
-  # For sufficiently-large n (n > 5040) if Riemann hypothesis true.
-  #
-  # So the lower bound for target T is:
-  # the first n for which e^\gamma n \log \log n > T
-  #
-  # n \log \log n > \frac{T}{e^\gamma}
-  #
-  # Since we are searching for a lower bound,
-  # we can increase \log \log n to \log \log T
-  #
-  # n > \frac{T}{e^\gamma \log \log T}
-  n = (target / (Math::E ** GAMMA * Math.log(Math.log(target)))).ceil
-
-  # That n was approximate. Binary search to get a better one?
-  n = (n..upper).bsearch { |i|
-    Math::E ** GAMMA * i * Math.log(Math.log(i)) >= target
-  }
-
-  # Eh, if n was less than 5040 we probably aren't gaining much anyway.
-  n > 5040 ? n : 1
-end
-
-def give_gifts(target, multiplier, elf_limit: nil)
-  elf_value_needed = (target / multiplier.to_f).ceil
-
-  # Find an upper bound on the house number.
-  # This reduces the work we need to do in the loop.
-  max_house = house_upper_bound(elf_value_needed, elf_limit: elf_limit)
-  gifts = Array.new(1 + max_house, 0)
-
-  min_house = house_lower_bound(elf_value_needed, max_house)
-
-  (1..max_house).each { |elf|
-    if elf < min_house
-      skipped = (min_house - 1) / elf
-      start_house = (skipped + 1) * elf
-    else
-      skipped = 0
-      start_house = elf
+      # subproblem: ceil(goal/prime_sum) using only primes less than prime
+      subgoal = (goal + prime_sum - 1) / prime_sum
+      best = [best, prime_power * sum_exceeds(subgoal, primes[1..])].min
     end
 
-    nums = (start_house..max_house).step(elf)
-    if elf_limit
-      next if skipped >= elf_limit
-      nums = nums.take(elf_limit - skipped)
-    end
-
-    nums.each_with_index { |house, i| gifts[house] += elf }
-
-    # If it's my first house, no later elf can undercut me.
-    return elf if elf >= min_house && gifts[elf] >= elf_value_needed
-  }
-
-  raise 'Impossible; some elf must have exceeded by now. Upper bound is wrong.'
+    best
+  end
 end
 
-# It's absoutely untenable to iterate every house and find its factors.
-# Just iterate the elves.
+puts house1 = sum_exceeds(target / 10)
 
-puts give_gifts(target, 10)
-puts give_gifts(target, 11, elf_limit: 50)
+# each elf only visits 50 houses,
+# so at most 50 elves visit a house: house / 1, house / 2, house / 3, etc.
+# so, testing a house is relatively fast.
+# And I'll go out on a limb and say that if a number isn't divisible by all of 2, 3, 5, 7,
+# it hasn't got much hope.
+[2, 3, 5, 7].each { |d| raise "unusual part 1 answer isn't divisible by #{d}" if house1 % d != 0 }
+good2 = ->house { 11 * (1..50).sum { |d| house % d == 0 ? house / d : 0 } >= target }
+puts (good2[house1] ? 0 : house1).step(by: 2 * 3 * 5 * 7).find(&good2)
